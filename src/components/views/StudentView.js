c1dbce1018da17b84c64ec99c757230b40ac7c49
David Chen <dav@Davids-MBP.lan>
2023-05-16 21:23:34 -0400

    Completed Single Student View

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -4,17 +4,25 @@ StudentView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display the single student view page.
 ================================================== */
+import { Link } from "react-router-dom";
+
 const StudentView = (props) => {
-  const { student } = props;
+  const { student, deleteStudent } = props;
 
   // Render a single Student view
   return (
     <div>
       <img src={student.imageUrl} style={{ width: '20%', height: '20%', objectFit: 'cover' }} alt="profile"></img>
       <h1>{student.firstname + " " + student.lastname}</h1>
-      <h3>{"College: " +student.campus.name}</h3>
+      {student.campusId ? (<Link to={`/campus/${student.campusId}`} ><h3>{"College: " +student.campus.name}</h3></Link>) : (<h3>This student is not enrolled</h3>)}
       <p>{"Email: " + student.email}</p>
       <p>{"GPA: " + student.gpa}</p>
+      <Link to={`/editstudent/` + student.id}>
+        <button>Edit Student</button>
+      </Link>
+      <Link to={'/students'}>
+        <button onClick={() => deleteStudent(student.id)}>Delete</button>
+      </Link>
     </div>
   );
 };
